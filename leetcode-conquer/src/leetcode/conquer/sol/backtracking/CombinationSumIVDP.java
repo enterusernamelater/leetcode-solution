f8b7038186405c6e5d24d55f3863323877b37ce9
Jay <jay.siyuan.he@gmail.com>
2019-05-14 00:26:49 -0400

    push 1 dp and recursion combination backtracking sol

---
+++ leetcode-conquer/src/leetcode/conquer/sol/backtracking/CombinationSumIVDP.java
@@ -0,0 +1,23 @@
+package leetcode.conquer.sol.backtracking;
+/*
+ * a dp solution that finds each target's difference and when the final target comes
+ * when just add all the smaller target records.
+ * time o(n*k), space o(k)
+ */
+public class CombinationSumIVDP {
+	
+	public CombinationSumIVDP() {}
+	
+    public int combinationSum4(int[] nums, int target) {
+        if(nums == null || nums.length == 0 || target == 0) return 0;
+        int[] arr = new int[target+1];
+        arr[0]=1;
+        for(int i=1;i<=target;i++)
+            for(int num : nums){
+                if(i-num >= 0){
+                    arr[i]+=arr[i-num];           
+            }
+        }
+        return arr[target];
+    }
+}
