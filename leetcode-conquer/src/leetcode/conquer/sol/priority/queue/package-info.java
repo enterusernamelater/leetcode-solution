174906aa83f8d53a5d08e35aa4638bc3a346fec9
Jay <jay.siyuan.he@gmail.com>
2019-08-15 19:44:26 -0400

    defination change

--- leetcode-conquer/src/leetcode/conquer/sol/priority/queue/package-info.java
+++ leetcode-conquer/src/leetcode/conquer/sol/priority/queue/package-info.java
@@ -1,2 +1,2 @@
-//priority queue can sort vals in the queue thats the biggest difference compare to the queue
+//priority queue can sort vals in the queue and poll or offer based on the sorting properties set upfront
 package leetcode.conquer.sol.priority.queue;
