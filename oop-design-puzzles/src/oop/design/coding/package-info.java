1134e5e6a382955a58a730d5788308077a73c6f9
Jay <jay.siyuan.he@gmail.com>
2020-07-07 20:46:09 -0400

    moving projects

--- oop-design-puzzles/src/oop/design/coding/package-info.java
+++
@@ -1,1 +0,0 @@
-package oop.design.coding;
--- oop-design-puzzles/src/oop/design/vending/machine/State.java
+++
@@ -1,9 +0,0 @@
-package oop.design.vending.machine;
-
-public interface State {
-	public void ready();
-	public void collectCash(int cash,String productCode);
-	public void pushChange(String productCode);
-	public void delivery(String productCode);
-	public void cancel();
-}
